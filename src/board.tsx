4d0a47195a8cae91ff9f82ccb25e45399d3d2257
takeshi0518 <y.takeshi0518@gmail.com>
2026-09-15 11:03:16 +0900

    feat: fill squares with X on click

--- src/board.tsx
+++ src/board.tsx
@@ -1,25 +1,46 @@
+import { useState } from 'react';
+
+type SquareValue = 'X' | '0' | null;
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array<SquareValue>(9).fill(null));
+
+  function handleClick(i: number) {
+    const nextSqures = squares.slice();
+    nextSqures[i] = 'X';
+    setSquares(nextSqures);
+  }
   return (
     <>
       <div>
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div>
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div>
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
 
-function Square({ value }: { value: string }) {
-  return <button className="square">{value}</button>;
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: SquareValue;
+  onSquareClick: () => void;
+}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
