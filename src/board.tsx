db82b2ec815dddcf8118f9d79e6dedfa21d7e956
takeshi0518 <y.takeshi0518@gmail.com>
2026-09-15 14:38:32 +0900

    feat: 勝者を判定して表示する

--- src/board.tsx
+++ src/board.tsx
@@ -6,8 +6,16 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array<SquareValue>(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
   function handleClick(i: number) {
-    if (squares[i]) {
+    if (squares[i] || winner) {
       return;
     }
     const nextSqures = squares.slice();
@@ -21,6 +29,7 @@ export default function Board() {
   }
   return (
     <>
+      <div>{status}</div>
       <div>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -52,4 +61,25 @@ function Square({
       {value}
     </button>
   );
+}
+
+function calculateWinner(squares: SquareValue[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
